990f57a11ee4a6924d0081251dc0c12d0f443213
Wanjiku <Wanjiku>
2019-07-31 23:16:54 +0300

    installed psycopg2

--- softwiz/settings.py
+++ softwiz/settings.py
@@ -95,8 +95,10 @@ WSGI_APPLICATION = 'softwiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'careers',
+        'USER': 'wanjiku',
+        'PASSWORD': 'mySql003',
     }
 }
 
